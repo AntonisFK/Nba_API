121de53be5aab20c7adaa4bbe2deab0ecc2964d8
Antonisfk <antonisfkiaras@gmail.com>
2016-04-14 20:45:59 -0700

    added bootstrap Ui

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 
 app.use(bodyParser.json()); 
 
+app.use(express.static(path.join(__dirname, './client')))
+
 require('./server/config/mongoose.js')
 require('./server/config/routes.js')(app);
 
